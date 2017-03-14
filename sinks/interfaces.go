79dcfdfeb12b303362ed6014ce42548c9908e3fe
Kevin Gessner <kgessner@etsy.com>
2017-03-14 20:35:30 +0000

    add a JSON sink, which is easier to parse than the glog output
    Signed-off-by: Kevin Gessner <kevin@kevingessner.com>

--- sinks/interfaces.go
+++ sinks/interfaces.go
@@ -38,6 +38,8 @@ func ManufactureSink() (e EventSinkInterface) {
 	switch s {
 	case "glog":
 		e = NewGlogSink()
+	case "json":
+		e = NewJSONSink()
 	// case "kafka"
 	// case "logfile"
 	default:
